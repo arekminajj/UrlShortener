401b364471308c219727ecdc6979d2ad30e8e721
Arek Cios <arek123113@gmail.com>
2020-08-01 09:56:48 +0200

    Added url model

---
+++ UrlShortener/Models/Url.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string IdString { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
